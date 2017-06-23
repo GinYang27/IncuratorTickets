901b56576cdf0f835997cd8d94f2a84ca59fa1ca
GinYang27 <gin.yang17@gmail.com>
2017-06-23 15:55:55 -0500

    Update MVP-1502.js

--- MVP-1502.js
+++ MVP-1502.js
@@ -1,1 +1,12 @@
+//I think sth usefull in this ticket is $route.reload()
+//It can update ng-view part only, maybe we can try it on login part
 
+$scope.$on('resetEplore',function(){
+    self.navbarSearchService.setTerm('');
+    $route.reload();
+});
+
+//https://stackoverflow.com/questions/5964373/is-there-a-difference-between-s-g-and-s-g
+if (searchTerm.replace(/\s+/g, "").length === 0) {
+  self.searchTerm = '';
+}
