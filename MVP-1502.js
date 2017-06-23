40549367cde35fe127120aac37dd7a987e9a76d1
GinYang27 <gin.yang17@gmail.com>
2017-06-23 16:02:11 -0500

    Update MVP-1502.js

--- MVP-1502.js
+++ MVP-1502.js
@@ -10,3 +10,31 @@ $scope.$on('resetEplore',function(){
 if (searchTerm.replace(/\s+/g, "").length === 0) {
   self.searchTerm = '';
 }
+
+/* Triggered by user search items
+ * Only trigger when flag changed
+ */
+function searchItems() {
+    if(self.searchChangedFlag){
+        self.itemIndex = 0; //!
+        self.exploreItems = [];
+        self.noItem = false;
+        getSearchItems(self.searchTerm, self.searchTypes, self.itemInterest, self.dealStatus, self.itemSortby, 0, self.totalItemsPerPage);
+        self.searchChangedFlag = false;
+        self.itemIndex = self.itemIndex + self.totalItemsPerPage; //!
+    } 
+}
+
+/**
+ * When click view more buttom, we will query more items from backend
+ */
+function viewMore() {
+    if (self.searchTerm !== '') {
+        self.endIndex = self.itemIndex + self.totalItemsPerPage;
+        getSearchItems(self.searchTerm, self.searchTypes, self.itemInterest, self.dealStatus, self.itemSortby, self.itemIndex, self.endIndex);
+    } else {
+        self.endIndex = self.itemIndex + self.totalItemsPerPage;
+        getDealPreviewBox(self.itemInterest, self.dealStatus, self.itemSortby, self.itemIndex, self.endIndex);
+    }
+    self.itemIndex = self.itemIndex + self.totalItemsPerPage;
+}
